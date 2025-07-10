681952089210fad29ff1481a7c044978e98ef407
yashineonline <yashineonline@gmail.com>
2025-07-10 14:19:15 -0400

    new structure

--- scripts/hf_push_model.sh
+++ scripts/hf_push_model.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-echo Uploading model
+# Set these variables
+SPACE_REPO_URL="https://huggingface.co/spaces/yashineonline/legal-hf"
+TEMP_DIR="temp_hf_space"
+
+
+
+# Clone the space repo
+git clone $SPACE_REPO_URL $TEMP_DIR
+
+# Copy your app files
+cp -r ../legal_hf/* .
+
+# Commit and push
+git add .
+git commit -m "Deploy legal_hf to Hugging Face Space"
+git push
+
+
